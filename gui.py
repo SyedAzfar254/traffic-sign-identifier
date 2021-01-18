7fd434628fc7fa6ce120dafa9ccfb472b1971db4
Ahmer Altaf <ahmeraltaf67@gmail.com>
2021-01-18 23:44:49 +0500

    upload image method

--- gui.py
+++ gui.py
@@ -2,3 +2,21 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter import *
 from PIL import ImageTk, Image
+
+
+
+
+
+def upload_image():
+    try:
+        file_path=filedialog.askopenfilename()
+        uploaded=Image.open(file_path)
+        uploaded.thumbnail(((top.winfo_width()/2.25),(top.winfo_height()/2.25)))
+        im=ImageTk.PhotoImage(uploaded)
+        
+        sign_image.configure(image=im)
+        sign_image.image=im
+        label.configure(text='')
+        show_classify_button(file_path)
+    except:
+        pass
