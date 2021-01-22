0207afea84da358f21e2f4e068908f282dd859d7
Ahmer Altaf <ahmeraltaf67@gmail.com>
2021-01-22 15:32:33 +0500

    adding imports and declare variables

---
+++ traffic_sign.py
@@ -0,0 +1,16 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import cv2
+import tensorflow as tf
+from PIL import Image
+import os
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+from keras.models import Sequential, load_model
+from keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout
+
+data = []
+labels = []
+classes = 43
+cur_path = os.getcwd()
