b8f6d3b131e4a38a40349ce8e52230c89c0b6b7a
SyedAzfar254 <syedazfar456@gmail.com>
2021-01-22 20:33:32 +0500

    Retriving Images and Converting Lists into Numpy arrays work done

--- traffic_sign.py
+++ traffic_sign.py
@@ -14,3 +14,29 @@ data = []
 labels = []
 classes = 43
 cur_path = os.getcwd()
+
+#Retrieving the images and their labels 
+for i in range(classes):
+    path = os.path.join(cur_path,'train',str(i))
+    images = os.listdir(path)
+
+    for a in images:
+        try:
+            image = Image.open(path + '\\'+ a)
+            image = image.resize((30,30))
+            image = np.array(image)
+            #sim = Image.fromarray(image)
+            data.append(image)
+            labels.append(i)
+        except:
+            print("Error loading image")
+
+#Converting lists into numpy arrays
+data = np.array(data)
+labels = np.array(labels)
+
+print(data.shape, labels.shape)
+#Splitting training and testing dataset
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
+
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
